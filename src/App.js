7960c5fc4703e69646ef3ad6336e116ce3df53be
Jonathan <jonathanbarreira@gmail.com>
2021-12-11 14:59:40 -0300

    fix: account error

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
       const accounts = await ethereum.request({ method: 'eth_accounts' });
 
       if (accounts.length !== 0) {
-        const account = account[0];
+        const account = accounts[0];
         console.log(`Found an authorized account: ${account}`);
         setCurrentAccount(account);
       } else {
