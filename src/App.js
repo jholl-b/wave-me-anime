e4afdff3dd8fc3c5208e93ce12692fe5c75272fc
Jonathan <jonathanbarreira@gmail.com>
2021-12-11 14:47:08 -0300

    feat: add wallet connection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,61 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+  
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+      if (accounts.length !== 0) {
+        const account = account[0];
+        console.log(`Found an authorized account: ${account}`);
+        setCurrentAccount(account);
+      } else {
+        console.log("No authorized account found");
+      }
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]); 
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   const wave = () => {
 
   }
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  });
 
   return (
     <div className="mainContainer">
@@ -23,6 +74,12 @@ function App() {
         <button className="waveButton" onClick={wave}>
           Me conta ai
         </button>
+
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Conectar Carteira
+          </button>
+        )}
       </div>
     </div>
   );
