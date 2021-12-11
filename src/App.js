37edf52935e1ef61192e879b6bf150bf2452a783
Jonathan <jonathanbarreira@gmail.com>
2021-12-11 18:03:47 -0300

    feat: Update contract

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import './App.css';
 
 function App() {
   const [currentAccount, setCurrentAccount] = useState("");
-  const [animeToSend, setAnimeToSend] = useState("");
+  const [allWaves, setAllWaves] = useState([]);
   const [animeWaved, setAnimeWaved] = useState(0);
-  const [nameAnimeWaved, setNameAnimeWaved] = useState("");
-  const contractAddress = "0x0D33F03F7Aab650F04383441F1ed8657adb42124";
+  const [animeToSend, setAnimeToSend] = useState("");
+  const contractAddress = "0x52ab4FC3f0223eDdF65390b6728B154A7a4977a8";
   const contractABI = Abi.abi;
 
   const checkIfWalletIsConnected = async () => {
@@ -32,6 +32,43 @@ function App() {
         console.log("No authorized account found");
       }
 
+      getAllWaves();
+      getTotalWaves();
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const getAllWaves = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+        const waves = await wavePortalContract.getAllWaves();
+
+        let wavesCleaned = [];
+        waves.forEach(wave => {
+          console.log(wave);
+          wavesCleaned.push({
+            address: wave.waver,
+            timestamp: new Date(wave.timestamp * 1000),
+            anime: wave.anime
+          });
+        });
+        console.log("-------------");
+        console.log(wavesCleaned);
+
+        if (wavesCleaned.length > 0) {
+          setAllWaves(wavesCleaned);
+        }
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+
     } catch (error) {
       console.log(error);
     }
@@ -48,27 +85,6 @@ function App() {
 
         let count = await wavePortalContract.getTotalWaves();
         setAnimeWaved(count.toNumber());
-        
-      } else {
-        console.log("Ethereum object doesn't exist!");
-      }
-
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  const getWavedAnime = async () => {
-    try {
-      const { ethereum } = window;
-
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
-        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
-
-        let anime = await wavePortalContract.getWavedAnime();
-        setNameAnimeWaved(anime);
         
       } else {
         console.log("Ethereum object doesn't exist!");
@@ -98,37 +114,8 @@ function App() {
     }
   }
 
-  // const wave = async () => {
-  //   try {
-  //     const { ethereum } = window;
-
-  //     if (ethereum) {
-  //       const provider = new ethers.providers.Web3Provider(ethereum);
-  //       const signer = provider.getSigner();
-  //       const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
-
-  //       let count = await wavePortalContract.getTotalWaves();
-  //       console.log(`Retrieved total wave count... ${count.toNumber()}`);
-
-  //       const waveTxn = await wavePortalContract.waveAnime("One Piece");
-  //       console.log("Mining...", waveTxn.hash);
-
-  //       await waveTxn.wait();
-  //       console.log("Mined -- ", waveTxn.hash);
-
-  //       count = await wavePortalContract.getTotalWaves();
-  //       console.log("Retrieved total wave count...", count.toNumber());
-        
-  //     } else {
-  //       console.log("Ethereum object doesn1t exist!");
-  //     }
-
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
-
   const waveAnime = async (e) => {
+    e.preventDefault();
 
     try {
       const { ethereum } = window;
@@ -138,7 +125,7 @@ function App() {
         const signer = provider.getSigner();
         const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
 
-        const waveTxn = await wavePortalContract.waveAnime(animeToSend);
+        const waveTxn = await wavePortalContract.wave(animeToSend);
         console.log("Mining...", waveTxn.hash);
 
         await waveTxn.wait();
@@ -155,14 +142,10 @@ function App() {
     } catch (error) {
       console.log(error);
     }
-
-    e.preventDefault();
   }
 
   useEffect(() => {
     checkIfWalletIsConnected();
-    getTotalWaves();
-    getWavedAnime();
   });
 
   return (
@@ -206,9 +189,17 @@ function App() {
         )}
 
         <div>
-          <h5>Você me sugeriu: {nameAnimeWaved}</h5>
           <h5>Total de sugestões: {animeWaved}</h5>
         </div>
+
+        {allWaves.map((wave, index) => {
+          return (
+            <div key={index} style={{ backgroundColor: "OldLace", marginTop: "16px", padding: "8px" }}>
+              <div>Address: {wave.address}</div>
+              <div>Time: {wave.timestamp.toString()}</div>
+              <div>Anime: {wave.anime}</div>
+            </div>)
+        })}
       </div>
     </div>
   );
